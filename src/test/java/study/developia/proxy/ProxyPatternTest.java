5227c9b95c75afbcf6099b44f94b43435301d3bd
developiaa <developiaa@gmail.com>
2022-04-04 22:23:52 +0900

    feat : Add proxy pattern test #2

--- src/test/java/study/developia/proxy/ProxyPatternTest.java
+++ src/test/java/study/developia/proxy/ProxyPatternTest.java
@@ -1,6 +1,7 @@
 package study.developia.proxy;
 
 import org.junit.jupiter.api.Test;
+import study.developia.proxy.code.CacheProxy;
 import study.developia.proxy.code.ProxyPatternClient;
 import study.developia.proxy.code.RealSubject;
 
@@ -13,5 +14,15 @@ public class ProxyPatternTest {
         proxyPatternClient.execute();
         proxyPatternClient.execute();
         proxyPatternClient.execute();
+    }
+
+    @Test
+    void cacheProxyTest() {
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+        client.execute();
+        client.execute();
+        client.execute();
     }
 }
