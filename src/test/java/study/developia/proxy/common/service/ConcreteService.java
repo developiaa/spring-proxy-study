852d93b7692f62c1e240f42fc17ec756243b1872
developiaa <developiaa@gmail.com>
2022-04-14 22:36:02 +0900

    feat : Add cglib test

---
+++ src/test/java/study/developia/proxy/common/service/ConcreteService.java
@@ -0,0 +1,10 @@
+package study.developia.proxy.common.service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ConcreteService {
+    public void call() {
+        log.info("ConcreteService 호출");
+    }
+}
---
+++ src/test/java/study/developia/proxy/common/service/ServiceInterface.java
@@ -0,0 +1,7 @@
+package study.developia.proxy.common.service;
+
+public interface ServiceInterface {
+    void save();
+
+    void find();
+}
