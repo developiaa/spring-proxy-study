657c4e5d330680d8a6bbad8e5a14b50b602e399d
developiaa <developiaa@gmail.com>
2022-04-15 23:30:40 +0900

    feat : Add proxy factory test

---
+++ src/test/java/study/developia/proxy/proxyfactory/ProxyFactoryTest.java
@@ -0,0 +1,35 @@
+package study.developia.proxy.proxyfactory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.AopUtils;
+import study.developia.proxy.common.advice.TimeAdvice;
+import study.developia.proxy.common.service.ServiceImpl;
+import study.developia.proxy.common.service.ServiceInterface;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+public class ProxyFactoryTest {
+
+    @Test
+    @DisplayName("인터페이스가 있으면 JDK 동적 프록시 사용")
+    void interfaceProxy() {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvice(new TimeAdvice());
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.save();
+
+        // 프록시가 정확히 이루어졌는지 확인
+        assertThat(AopUtils.isAopProxy(proxy)).isTrue();
+        assertThat(AopUtils.isJdkDynamicProxy(proxy)).isTrue();
+        assertThat(AopUtils.isCglibProxy(proxy)).isFalse();
+    }
+}
